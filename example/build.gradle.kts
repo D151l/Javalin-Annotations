afeca285edd712cc582a92d97afbb4b4a8681a32
D151l <d151l@d151l.de>
2022-12-25 13:30:54 +0100

    fix example

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 }
 
 dependencies {
+    implementation("io.javalin:javalin:5.2.0")
     implementation("com.github.D151l:Javalin-Annotations:868f9de2e3")
     implementation("org.slf4j:slf4j-simple:2.0.3")
 }
