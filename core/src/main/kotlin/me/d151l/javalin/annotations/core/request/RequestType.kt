83d469464692c62daf307051112d5dddc73b9f66
D151l <d151l@d151l.de>
2022-12-27 19:30:11 +0100

    starting white request types

---
+++ core/src/main/kotlin/me/d151l/javalin/annotations/core/request/RequestType.kt
@@ -0,0 +1,13 @@
+package me.d151l.javalin.annotations.core.request
+
+/**
+ * @created 27/12/2022 - 19:09
+ * @project Javalin-Annotations
+ * @author  D151l
+ */
+enum class RequestType {
+    GET,
+    POST,
+    PUT,
+    DELETE
+}
